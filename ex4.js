2fe8e8748a41123d34567a2bfd598b1e4828aa38
yara <yaralm5@hotmail.com>
2024-11-01 15:48:13 -0300

    ex4

--- ex4.js
+++ ex4.js
@@ -1,14 +1,36 @@
 const form = document.querySelector("form"); 
 const mostrarfila = document.getElementById("fila").innerHTML; 
+
 let fila = []; 
+let atend = []; 
 
 
 function incluirFila() {
     let npac = document.getElementById("pac").value; 
 
-        fila.push(npac); 
+        fila.push(npac);
         return fila.join('<br>'); 
        
     
 }
 
+function Prioritario(){
+    let npac = document.getElementById("pac").value; 
+
+    fila.unshift(npac); 
+    return fila.join('<br>'); 
+
+}
+
+function Atendimento(){
+    let npac = document.getElementById("pac").value; 
+    
+    for(let i of fila){
+        if (npac == i){
+            atend[0] = npac; 
+            fila.shift(npac); 
+        }
+    }
+
+    return atend.join('<br>'); 
+}
