9ecde15ec35bf896e53c9802bdf883049de1fc55
yara <yaralm5@hotmail.com>
2024-11-01 22:16:38 -0300

    começando

---
+++ EXFIX5/JS/JOGOJS.js
@@ -0,0 +1,6 @@
+function palavraAleatoria(){
+    const palavras = ['polia', 'terço', 'polen', 'pauta', 'santo', 'conto', 'rolon', 'feito', 'broca', 'jaula']; 
+    const senha = palavras[Math.floor(Math.random * 10)].toUpperCase(); 
+
+    
+}
